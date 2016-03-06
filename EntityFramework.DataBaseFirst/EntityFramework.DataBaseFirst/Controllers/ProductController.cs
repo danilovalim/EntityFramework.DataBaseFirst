aa9afaf97447b4dd78be6fd25776b47df9b246c2
Danilo Valim <danilovalim@hotmail.com>
2016-03-06 17:12:26 -0300

    First Commit

---
+++ EntityFramework.DataBaseFirst/EntityFramework.DataBaseFirst/Controllers/ProductController.cs
@@ -0,0 +1,105 @@
+﻿using EntityFramework.DataBaseFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET.MVC.EF.CodeFirst.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            using (StoreContext db = new StoreContext())
+            {
+                var products = db.Products.ToList();
+                return View(products);
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View(new Product());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                using (StoreContext db = new StoreContext())
+                {
+                    db.Products.Add(p);
+                    db.SaveChanges();
+                    return RedirectToAction("List");
+                }
+            }
+            else
+            {
+                return View(p);
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            using (StoreContext db = new StoreContext())
+            {
+                var prod = db.Products.Find(id);
+                return View(prod);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product p)
+        {
+            using (StoreContext db = new StoreContext())
+            {
+                var prod = db.Products.Where(x => x.ProductID == p.ProductID).First();
+                prod.Name = p.Name;
+                prod.Description = p.Description;
+                prod.Price = p.Price;
+                db.SaveChanges();
+                return RedirectToAction("List");
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (StoreContext db = new StoreContext())
+            {
+                Product prod = db.Products.Find(id);
+                return View(prod);
+            }
+        }
+           
+
+
+        public ActionResult Delete(int id)
+        {
+            using (StoreContext db = new StoreContext())
+            {
+                Product prod = db.Products.Find(id);
+                return View(prod);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Product p, int id)
+        {
+            using (StoreContext db = new StoreContext())
+            {
+                Product prod = db.Products.Find(id);
+                db.Products.Remove(prod);
+                db.SaveChanges();
+                return RedirectToAction("List");
+            }
+        }
+    }
+}
---
+++ EntityFramework.DataBaseFirst/EntityFramework.DataBaseFirst/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EntityFramework.DataBaseFirst.MvcApplication" Language="C#" %>
